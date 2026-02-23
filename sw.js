c137ac6a8f6146660a7e5a6f5b6713d3426e8562
nextwave-nz <hello@nextwave.nz>
2026-02-24 00:04:09 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,35 @@
-const CACHE_NAME = 'ramadan-hamilton-2026-v7';
+const CACHE_NAME = 'ramadan-hamilton-2026-v8';
 const urlsToCache = [
-'./',
-'./index.html',
-'./manifest.json',
-'./ramadan-hamilton-2026.ics'
+    './',
+    './index.html',
+    './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
-event.waitUntil(
-  caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-);
-self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
-event.respondWith(
-  caches.match(event.request).then((response) => response || fetch(event.request))
-);
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
 });
 
 self.addEventListener('activate', (event) => {
-const cacheWhitelist = [CACHE_NAME];
-event.waitUntil(
-  caches.keys().then((cacheNames) => {
-    return Promise.all(
-      cacheNames.map((cacheName) => {
-        if (cacheWhitelist.indexOf(cacheName) === -1) {
-          return caches.delete(cacheName);
-        }
-      })
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
-  })
-);
-self.clients.claim();
+    self.clients.claim();
 });
