8e2aa04aabeb91247ec06139a822f6a15583aa20
nextwave-nz <hello@nextwave.nz>
2026-02-23 23:35:18 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,51 +1,24 @@
-const CACHE_NAME = 'ramadan-hamilton-2026-v4';
+const CACHE_NAME = 'ramadan-hamilton-2026-v7';
 const urlsToCache = [
 './',
 './index.html',
-'./manifest.json'
+'./manifest.json',
+'./ramadan-hamilton-2026.ics'
 ];
 
-// Install Service Worker
 self.addEventListener('install', (event) => {
 event.waitUntil(
-  caches.open(CACHE_NAME)
-    .then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    })
+  caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
 );
 self.skipWaiting();
 });
 
-// Cache and return requests
 self.addEventListener('fetch', (event) => {
 event.respondWith(
-  caches.match(event.request)
-    .then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request).then(
-        (response) => {
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          const responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        }
-      );
-    })
+  caches.match(event.request).then((response) => response || fetch(event.request))
 );
 });
 
-// Update Service Worker
 self.addEventListener('activate', (event) => {
 const cacheWhitelist = [CACHE_NAME];
 event.waitUntil(
