78e5a9f440be0a7e473f8822cdc689b244c523b4
nextwave-nz <hello@nextwave.nz>
2026-02-23 17:13:39 +1300

    Update sw.js

--- sw.js
+++ sw.js
@@ -22,18 +22,15 @@ self.addEventListener('fetch', (event) => {
 event.respondWith(
   caches.match(event.request)
     .then((response) => {
-      // Cache hit - return response
       if (response) {
         return response;
       }
       return fetch(event.request).then(
         (response) => {
-          // Check if valid response
           if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
           }
 
-          // Clone the response
           const responseToCache = response.clone();
 
           caches.open(CACHE_NAME)
